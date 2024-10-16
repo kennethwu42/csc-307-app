c08b00975fd0f9e82021d93f3e6a2a9e69e7a44c
kennethwu42 <kwu45@calpoly.edu>
2024-10-16 13:10:55 -0700

    IE4

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,39 +1,71 @@
 // src/MyApp.jsx
-import React from "react";
-import { useState } from "react";
+import React, {useState, useEffect} from 'react';
 import Table from "./Table";
 import Form from "./Form";
 
-
-  
 function MyApp() {
-    const [characters, setCharacters] = useState([]);
 
-    function updateList(person) {
-        setCharacters([...characters, person]);
-      }
-    
-    function removeOneCharacter(index) {
-      const updated = characters.filter((character, i) => {
-        return i !== index;
-      });
-      setCharacters(updated);
-    }
+  // state is like a shopping cart. You can add or remove items from it
+  const [characters, setCharacters] = useState([]);
 
-    function updateList(person) {
-        setCharacters([...characters, person]);
-      }
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => { console.log(error); });
+  }, [] );
 
-    
-    return (
-        <div className="container">
-          <Table
-            characterData={characters}
-            removeCharacter={removeOneCharacter}
-          />
-          <Form handleSubmit={updateList} />
-        </div>
-      );
+  return (
+    <div className="container">
+      <Table 
+        characterData={characters} 
+        removeCharacter={removeOneCharacter}
+      />
+      <Form handleSubmit={updateList} />
+    </div>
+  );
+
+  function updateList(person){ 
+    postUser(person)
+      .then((res) => res.status == 201 ? res.json() : undefined)
+      .then((new_character) => setCharacters([...characters, new_character]))
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  function removeOneCharacter(index){
+    const character_id = characters[index].id;
+    deleteUser(character_id);
+    const updated_list = characters.filter((characters, i) => {
+      return i !== index;
+    });
+    setCharacters(updated_list);
+  }
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  } 
+
+  function postUser(person) {
+    const promise = fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+  function deleteUser(id){
+    fetch(`http://localhost:8000/users/${id}`, {
+      method: "DELETE",
+    })
+  }
+  
 }
+
 
 export default MyApp;
